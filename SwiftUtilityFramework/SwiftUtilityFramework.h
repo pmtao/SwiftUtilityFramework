80dc815aa549ca15423145a5f46d70c2585cb1bd
Meler Paine <pmtnmd@163.com>
2017-08-16 09:42:02 +0800

    Initial Commit

---
+++ SwiftUtilityFramework/SwiftUtilityFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtilityFramework.h
+//  SwiftUtilityFramework
+//
+//  Created by 阿涛 on 17-8-16.
+//  Copyright © 2017年 SinkingSoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtilityFramework.
+FOUNDATION_EXPORT double SwiftUtilityFrameworkVersionNumber;
+
+//! Project version string for SwiftUtilityFramework.
+FOUNDATION_EXPORT const unsigned char SwiftUtilityFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtilityFramework/PublicHeader.h>
+
+

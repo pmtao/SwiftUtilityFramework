442a3d5727779806f96ccad29e5c15a5b0c772b7
Meler Paine <pmtnmd@gmail.com>
2019-03-04 17:00:35 +0800

    1. TextTagger 类，新增方法 analyzeEnglishSentence，分解英文句子提取详细信息，返回为元组类型的数组，元组中依次为原始单词、单词的词根、单词所在句子的范围。 2. TextTagger 类，修改 getEnglishSentenceLemma 方法，优化对 can't 分词的错误。 ----- 提交人：镜画者，发现有任何问题请联系我：pmtnmd@gmail.com。 Commit by Meler Paine, found any problem please contact: pmtnmd@gmail.com.

--- SwiftUtilityFramework/Foundation/Text/TextTagger.swift
+++ SwiftUtilityFramework/Foundation/Text/TextTagger.swift
@@ -42,16 +42,65 @@ public class TextTagger {
     
     /// 提取英文句子中所有单词的词根
     public static func getEnglishSentenceLemma(_ sentence: String) -> [String] {
-        var lemmas: [String] = []
+        var lemmas: [String?] = []
+        var originWords: [String] = []
         let range = NSRange(location: 0, length: sentence.count)
         let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace]
         let orthography = NSOrthography(dominantScript: "Latn", languageMap: ["Latn": ["en"]])
         (sentence as NSString).enumerateLinguisticTags(in: range, scheme: .lemma, options: options, orthography: orthography) { (tag, tokenRange, sentenceRange, _) in
-            if let lemma = tag?.rawValue {
-                lemmas.append(lemma)
+            let originWord = (sentence as NSString).substring(with: tokenRange)
+            lemmas.append(tag?.rawValue)
+            originWords.append(originWord)
+            // 结果修正
+            // 解决分词可能存在的问题。
+            // 情况1: 如 can't 分词时，被拆分为: 'ca', 'n't'，正确的应该是: 'can', 'n't'.
+            if lemmas.count >= 2 {
+                // 情况1:
+                let indexNeedToFixed = lemmas.count - 2
+                if originWord == "n't" && originWords[indexNeedToFixed] == "ca" {
+                    var newItem = lemmas[indexNeedToFixed]
+                    newItem = "can"
+                    lemmas[indexNeedToFixed] = newItem
+                }
             }
         }
-        return lemmas
+        
+        return lemmas.filter { $0 != nil } as! [String] // 去除结果不为 nil 的项
+    }
+    
+    /// 分解英文句子提取详细信息，返回为元组类型的数组，元组中依次为原始单词、单词的词根、单词所在句子的范围。
+    /// - 当提取的词根为 nil 时，对原词为以下情况的需要忽略：纯数字，带标点的单词缩写:'ll 've 等。
+    public static func analyzeEnglishSentence(_ sentence: String) -> [(String, String?, NSRange)] {
+        var result: [(String, String?, NSRange)] = []
+        let range = NSRange(location: 0, length: sentence.count)
+        let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .omitOther, .joinNames]
+        let orthography = NSOrthography(dominantScript: "Latn", languageMap: ["Latn": ["en"]])
+        (sentence as NSString).enumerateLinguisticTags(in: range, scheme: .lemma, options: options, orthography: orthography) { (tag, tokenRange, sentenceRange, _) in
+            let originWord = (sentence as NSString).substring(with: tokenRange)
+            let resultItem = (originWord, tag?.rawValue, sentenceRange)
+            if tag == nil {
+                // 词根为空时的处理
+                let numberCharSet = CharacterSet(charactersIn: "0123456789")
+                let wordSet = CharacterSet(charactersIn: originWord)
+                if (wordSet.isSubset(of: numberCharSet) || originWord.contains("'")) == false {
+                    result.append(resultItem)
+                }
+            } else {
+                result.append(resultItem)
+            }
+            // 结果修正
+            // 解决分词可能存在的问题。
+            // 情况1: 如 can't 分词时，被拆分为: 'ca', 'n't'，正确的应该是: 'can', 'n't'.
+            if result.count >= 2 {
+                // 情况1:
+                let indexNeedToFixed = result.count - 2
+                if originWord == "n't" && result[indexNeedToFixed].0 == "ca" {
+                    let item = result[indexNeedToFixed]
+                    result[indexNeedToFixed] = ("can", "can", item.2)
+                }
+            }
+        }
+        return result
     }
     
 }
